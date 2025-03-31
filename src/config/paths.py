2d4a36fc22cb2b3b0f96b4a42f56d54d61eca7f4
fefedelcid <thegreench1996@gmail.com>
2025-03-31 16:12:24 -0300

    cambios de path y notificaciones

--- src/config/paths.py
+++ src/config/paths.py
@@ -8,6 +8,7 @@ VENV_PATH = os.path.join(".venv", "Scripts", "python.exe") if os.name == "nt" el
 
 # Rutas importantes
 CONFIG_DIR = os.path.join(BASE_DIR, "config")
+SESSION_DIR = os.path.join(BASE_DIR, "core/session")
 VERSION_FILE = os.path.join(BASE_DIR, "../version.json")
 SECRETS_DIR = os.path.join(CONFIG_DIR, "secrets")
 CREDENTIALS_PATH = os.path.join(SECRETS_DIR, "credentials.json")
@@ -22,7 +23,8 @@ UPDATE_URL = "https://raw.githubusercontent.com/fefedelcid/gym_manager/refs/head
 # Verificación de existencia de credenciales
 if not os.path.exists(CREDENTIALS_PATH):
     raise FileNotFoundError(f"El archivo de credenciales no se encuentra en {CREDENTIALS_PATH}")
-
+if not os.path.exists(SESSION_DIR):
+    os.mkdir(SESSION_DIR)
 
 # Alcance de la app
 SCOPES = [
--- src/core/notificaciones.py
+++ src/core/notificaciones.py
@@ -9,14 +9,20 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 
 # Aditional imports
+import os, time
 from urllib.parse import quote
 # from src.database import Database
+# from src.config import SESSION_DIR
 from datetime import datetime, timedelta
 
+SESSION_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "session")
+if not os.path.exists(SESSION_DIR):
+    os.mkdir(SESSION_DIR)
 
 class KatzupBot:
     driver: Chrome = None
     options: ChromeOptions = None
+    sender: str = ""
     # _database: Database = None
 
     def __init__(self):
@@ -31,8 +37,8 @@ class KatzupBot:
         self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
         self.options.add_argument("--disable-dev-shm-usage")
         self.options.add_argument("--mute-audio")
-        self.options.add_argument("--kiosk")
-        self.options.add_argument("user-data-dir=./session")
+        # self.options.add_argument("--kiosk")
+        self.options.add_argument(f"user-data-dir={SESSION_DIR}")
         self.options.add_argument("--no-sandbox")
 
 
@@ -48,19 +54,44 @@ class KatzupBot:
         wait = self.__generate_wait(60)
 
         try:
-            wait.until(
-                EC.visibility_of_element_located(
-                    (By.CSS_SELECTOR, 'span[data-icon="lock-small"]')
-                )
-            )
+            # wait.until(
+            #     EC.visibility_of_element_located(
+            #         (By.CSS_SELECTOR, 'span[data-icon="lock-small"]')
+            #     )
+            # )
+            time.sleep(60)
         except TimeoutException as e:
             print(e)
             print("[ERROR] Tiempo de espera excedido.")
+        finally:
             self.quit()
+        
+    def __get_chat_input(self):
+        wait = self.__generate_wait(20)
+        chat = wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, 'div[aria-placeholder="Escribe un mensaje"]')
+            )
+        )
+        return chat
 
+    def __select_my_chat__(self):
+        wait = self.__generate_wait(20)
+        cuadro_buscar = wait.until(
+            EC.visibility_of_element_located(
+                (By.CSS_SELECTOR, 'div[aria-label="Buscar"]')
+            )
+        )
+        cuadro_buscar.click()
+        cuadro_buscar.send_keys(self.sender+"\n")
+
+    
     def run(self):
         print("[INFO] Iniciando WebDriver.")
         self.__init_webdriver()
+
+    def quit(self):
+        self.driver.quit()
         
 
 if __name__=="__main__":
